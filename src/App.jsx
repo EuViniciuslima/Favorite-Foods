9cd2a069db8cccaa0a5b12a603c8c791bcf5e8de
Vinicius <viniciuslimavi05@gmail.com>
2023-12-05 21:06:25 -0300

    Atualização de aparecer o criar login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import './styles.css'
-import imagem from './assets/ff.svg'
 import { useState } from 'react';
-import './CreateAccount';  
-
-
+import './styles.css';
+import imagem from './assets/ff.svg';
+import CreateAccount from './CreateAccount'; // Importe o componente de criação de conta
 
 function App() {
-  const [setShowCreateAccount] = useState(false);  
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleCreateAccountClick = () => {
     setShowCreateAccount(true);
   };
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
+  const handleCreateAccount = (newUser) => {
+    // Aqui você pode adicionar a lógica para lidar com o novo usuário criado
+    console.log("Novo usuário cadastrado:", newUser);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,44 +40,48 @@ function App() {
 
   return (
     <div className="container">
-      <div className="container-login">
-        <div className="wrap-login">
-          <form className="login-form" onSubmit={handleSubmit}>
-            <span className="login-form-title">Bem vindo!</span>
-            <span className="login-form-title">
-              <img src={imagem} alt="favorite-foods" />
-            </span>
-            <div className='wrap-input'>
-              <input
-                className={email !== "" ? 'has-val input' : 'input'}
-                type='email'
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-              <span className='focus-input' data-placeholder='Email'></span>
-            </div>
+      {showCreateAccount ? (
+        <CreateAccount handleCreateAccount={handleCreateAccount} />
+      ) : (
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <span className="login-form-title">Bem vindo!</span>
+              <span className="login-form-title">
+                <img src={imagem} alt="favorite-foods" />
+              </span>
+              <div className='wrap-input'>
+                <input
+                  className={email !== "" ? 'has-val input' : 'input'}
+                  type='email'
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <span className='focus-input' data-placeholder='Email'></span>
+              </div>
 
-            <div className='wrap-input'>
-              <input
-                className={password !== "" ? 'has-val input' : 'input'}
-                type='password'
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-              <span className='focus-input' data-placeholder='Password'></span>
-            </div>
-            <div className='container-login-form-btn'>
-              <button className='login-form-btn'>login</button>
-            </div>
-            <div className='text-center'>
-              <span className='text1'> Não possui conta?</span>
-              <a className='text2' href='create-account' onClick={handleCreateAccountClick}>
-              Criar Conta
-              </a>
-            </div>
-          </form>
+              <div className='wrap-input'>
+                <input
+                  className={password !== "" ? 'has-val input' : 'input'}
+                  type='password'
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <span className='focus-input' data-placeholder='Password'></span>
+              </div>
+              <div className='container-login-form-btn'>
+                <button className='login-form-btn'>login</button>
+              </div>
+              <div className='text-center'>
+                <span className='text1'> Não possui conta?</span>
+                <button className='text2' onClick={handleCreateAccountClick}>
+                  Criar Conta
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
